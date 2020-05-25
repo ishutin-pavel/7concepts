a7177685f3ee7917d5112676ce8f64aefe13d26e
Ishutin Pavel <ishutin-pavel@mail.ru>
2020-05-25 22:34:27 +0300

    update owl-carousel in functions.php

--- functions.php
+++ functions.php
@@ -61,21 +61,19 @@ add_action( 'widgets_init', 'concepts_widgets_init' );
 function concepts_scripts() {
 	wp_enqueue_style( 'raleway', get_template_directory_uri() . '/fonts/raleway/stylesheet.css' );
 	wp_enqueue_style( 'roboto', get_template_directory_uri() . '/fonts/roboto/stylesheet.css' );
-	wp_enqueue_style( 'owl-carousel-css', get_template_directory_uri() . '/libs/OwlCarousel2/dist/assets/owl.carousel.css' );
-	wp_enqueue_style( 'owl-theme', get_template_directory_uri() . '/libs/OwlCarousel2/dist/assets/owl.theme.default.css' );
+	wp_enqueue_style( 'owl-carousel-css', get_template_directory_uri() . '/libs/owl-carousel/owl.carousel.css' );
+	wp_enqueue_style( 'owl-theme', get_template_directory_uri() . '/libs/owl-carousel/owl.theme.default.css' );
 	wp_enqueue_style( 'fancybox-css', get_template_directory_uri() . '/libs/fancybox/jquery.fancybox.css' );
 	wp_enqueue_style( 'modal-css', get_template_directory_uri() . '/libs/jquery-modal/jquery.modal.css' );
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/libs/bootstrap/css/bootstrap.min.css' );
 	wp_enqueue_style( 'concepts-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'concepts-main', get_template_directory_uri() . '/css/main.css' );
-
 
 	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/libs/jQuery/jquery-3.3.1.min.js', array(), null, true );
 	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/libs/bootstrap/js/bootstrap.min.js', array(), null, true );
 	wp_enqueue_script( 'pagescroll2id', get_template_directory_uri() . '/libs/pagescroll2id/jquery.malihu.PageScroll2id.js', array(), null, true );
 	wp_enqueue_script( 'fancybox-js', get_template_directory_uri() . '/libs/fancybox/jquery.fancybox.js', array(), '20151215', true );
 	wp_enqueue_script( 'mousewheel', get_template_directory_uri() . '/libs/jQuery/jquery.mousewheel-3.0.6.pack.js', array(), null, true );
-	wp_enqueue_script( 'owl-carousel-js', get_template_directory_uri() . '/libs/OwlCarousel2/dist/owl.carousel.min.js', array('jquery'), null, true );
+	wp_enqueue_script( 'owl-carousel-js', get_template_directory_uri() . '/libs/owl-carousel/owl.carousel.min.js', array('jquery'), null, true );
 	wp_enqueue_script( 'modal-js', get_template_directory_uri() . '/libs/jquery-modal/jquery.modal.min.js', array(), null, true );
 	wp_enqueue_script( 'maskedinput', get_template_directory_uri() . '/libs/maskedinput/jquery.maskedinput.min.js', array(), null, true );
 	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/js/main.js', array(), null, true );
